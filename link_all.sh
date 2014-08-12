b35f9a8e90aada86669668e4f2944da49475f96f
Garrett Berg <gberg@neutral.io>
2014-08-12 14:35:10 -0600

    updated link

--- link_all.sh
+++ link_all.sh
@@ -1,5 +1,5 @@
 cd ~
-ln -s .spf13-vim-3/.bash_profile
-ln -s .bashrc .spf13-vim-3/.bashrc
-ln -s .tmux.conf .spf13-vim-3/.tmux.conf
+ln -s .spf13-vim-3/.bash_profile .bash_profile
+ln -s .spf13-vim-3/.bashrc .bashrc 
+ln -s .spf13-vim-3/.tmux.conf .tmux.conf 
 
