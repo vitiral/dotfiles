7b18978a6c11238868467f49f86981413cfe8ac2
Garrett Berg <gberg@neutral.io>
2015-03-03 14:07:46 -0700

    pythonrc: added database, local and cwd configurations

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,8 +1,11 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-loaded = False  # used to detect tryimp loading
-rcloaded = False
+# user, user local, and cwd configurations
+# stored as ~/.pythonrc.py, ~/.pythonrc_local.py, cwd/interactive.py
+pythonrc, localrc, interactiverc = False, False, False
+
+loaded, lp, config = (None,) * 3  # deleted at end
 
 try:
     __IPYTHON__
@@ -10,37 +13,60 @@ try:
 except NameError:
     pass
 
-# python2 interface
+##################################################
+# ## basic namespace
+
 import sys
 import os
+import configparser
+from imp import reload
+rl = reload
 
-# python 2 compatibility
-if sys.version_info.major < 3:
-    range = xrange
-    del xrange
 
-# basic
 import re
 import pprint
 path = os.path
 psplit = path.split
+abspath = lambda p: path.abspath(path.expanduser(p))
 p = print
 pp = pprint.pprint
 
-# basic scientific libraries
+
+def filestr(filepath):
+    '''read file contents'''
+    filepath = abspath(filepath)
+    with open(filepath, 'r') as f:
+        return f.read()
+
+
+##################################################
+# ## python2 compatibility (more to be added)
+if sys.version_info.major < 3:
+    range = xrange
+    del xrange
+
+
+##################################################
+# ## basic scientific libraries
+
+import math
+import statistics
+stats = statistics
 
 # data to play with
 r = range(10)
 l = list(r)
 d = {key: key for key in r}
 s = set(r)
-tryimp = '''  # exec lines to try importing modules into namespace
+
+# Load optional scientific packages
+tryimp = '''
 try:
     loaded = False
     import {0}
     loaded = True
 except ImportError:
-    print(' - Module "{0}" could not be loaded')
+    print('[WARN ] Module "{0}" could not be loaded')
 '''.format
 
 exec(tryimp('numpy as np'))
@@ -53,15 +79,46 @@ if loaded:
 exec(tryimp('bokeh'))
 if loaded:
     import bokeh.plotting as plt
-
-import math
-import statistics
-stats = statistics
-
-# toolbox
 exec(tryimp("cloudtb as tb"))
 if loaded:
-    from cloudtb.builtin import *  # import should be builtins
+    from cloudtb.builtin import *
 
-rcloaded = True
-del loaded
+
+##################################################
+# ## database tools
+exec(tryimp('pymongo'))
+if loaded:
+    try:
+        lp = abspath("~/.secret/mongo.cfg")
+        config = configparser.ConfigParser()
+        config.read(lp)
+        lp = dict(config['creds'])
+        client = pymongo.MongoClient(lp['host'], int(lp['port']))
+        db = client[lp['database']]
+        db.authenticate(lp['username'], lp['password'],
+                        source=lp['source'])
+    except Exception as e:
+        print("[WARN ] Could not completely load mongo database {}: {}".
+              format(type(E), E))
+
+
+##################################################
+# ## load local and cwd configs
+try:
+    # import ipdb; ipdb.set_trace()
+    lp = "~/.pythonrc_local.py"
+    exec(filestr(lp))
+    print("[OK   ] loaded {}".format(lp))
+    localrc = True
+except: pass
+try:
+    exec(filestr("interactive.py"))
+    print("[OK   ] loaded cwd/interactive.py")
+    interactiverc = True
+except: pass
+
+
+##################################################
+# ## cleanup
+pythonrc = True
+del loaded, lp, config
