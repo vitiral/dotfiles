dd72e83df61fbcd46ac163140e96b163ad11f59d
Garrett Berg <googberg@gmail.com>
2015-08-03 12:28:59 -0600

    bug in insall

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,43 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 mkdir ~/projects ~/bin ~/software
-source "$SCRIPTPATH/git_setup.sh"
+
+if [[ `uname` == "Linux" ]]; then
+    echo "Is Linux"
+    OSNAME=`cat /etc/*-release | grep ^NAME=`
+fi
+
+# Get the base directory if it doesn't exist
+cd ~
+if [[ ! -e ~/.dotfiles ]]; then
+    if [[ `uname` == "Linux" ]]; then
+        if [[ "Ubuntu" == *"$(OSNAME)"* ]]; then
+            sudo apt-get install -y git
+        fi
+    fi
+    git clone https://github.com/cloudformdesign/dotfiles.git ~/.dotfiles && ~/.dotfiles/install/install.sh
+fi
+
+# setup git
+git config --global user.email "googberg@gmail.com"
+git config --global user.name "Garrett Berg"
+git config --global core.editor "vim"
+
+if [[ ! -e ~/.ssh/id_rsa.pub ]]; then
+    echo "Press enter for EVERYTHING"
+    ssh-keygen -t rsa -b 4096 -C "googberg@gmail.com"
+    echo "Go to https://github.com/settings/ssh and paste. Press ENTER when done."
+    cat ~/.ssh/id_rsa.pub
+    read
+fi
+eval "$(ssh-agent -s)"
+ssh git@github.com  # should confirm that everything worked
+cd ~/.dotfiles
+git remote rm origin
+git remote add origin git@github.com:cloudformdesign/dotfiles.git
+
+cd ~
+git clone git@github.com:cloudformdesign/notes.git
 
 # Install useful software
 if [[ `uname` == "Darwin" ]]; then
@@ -10,8 +46,6 @@ if [[ `uname` == "Darwin" ]]; then
     source "$SCRIPTPATH/install_mac.sh"
     source "$SCRIPTPATH/install_linux.sh"
 elif [[ `uname` == "Linux" ]]; then
-    echo "Is Linux"
-    OSNAME=`cat /etc/*-release | grep ^NAME=`
     if [[ "Ubuntu" == *"$(OSNAME)"* ]]; then
         echo "  Is Ubuntu"
         source "$SCRIPTPATH/install_ubuntu.sh"
