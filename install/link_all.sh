f513166ee469a756b70c209c95891502d0892336
Garrett Berg <googberg@gmail.com>
2015-08-31 22:59:17 +0000

    new configs

--- install/link_all.sh
+++ install/link_all.sh
@@ -1,4 +1,7 @@
+
+# std dotfiles
 cd ~
+ln -s .dotfiles/.Xresources .
 ln -s .dotfiles/.alias .
 ln -s .dotfiles/.bash_profile .
 ln -s .dotfiles/.bashrc .
@@ -6,8 +9,13 @@ ln -s .dotfiles/.tmux.conf .
 ln -s .dotfiles/.zshrc .
 ln -s .dotfiles/.vimperatorrc .
 
-# ipyython
+# .config
+mkdir -p .config
+cd .config
+ln -s ~/.dotfiles/config/i3 .
+
+# python
 cd ~
 ln -s .dotfiles/.pythonrc.py
-cd ~/.ipython/profile_default/ && ln -s ~/.spf13-vim-3/ipython_config.py
+cd ~/.ipython/profile_default/ && ln -s ~/.dotfiles/ipython_config.py
 cd ~
