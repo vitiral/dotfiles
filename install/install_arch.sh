dfba89a4e1f96439bef89ce38d302d7cb4a5e179
Garrett Berg <googberg@gmail.com>
2015-09-01 22:18:37 +0000

    more arch

--- install/install_arch.sh
+++ install/install_arch.sh
@@ -4,58 +4,78 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 SYS_INSTALL="sudo pacman -S --noconfir --needed --ignore all"
 CREATE_USER=garrett
 
+# system settings
+sudo cp $SCRIPTPATH/etc/99-sysctl.confg /etc/sysctl.d/  # swappiness
+sudo cp $SCRIPTPATH/etc/50-synaptics.conf /etc/X11/xorg.conf.d  # touchpad
+
+if [[ ! -e /etc/locale.conf ]]; then
+    sudo cp $SCRIPTPATH/etc/locale.gen /etc
+    sudo locale-gen
+    sudo cp $SCRIPTPATH/etc/locale.conf /etc
+fi
+
 if [[ ! -e /etc/localtime ]]; then
-    ln -sf /usr/share/zoneinfo/Zone/SubZone /etc/localtime
-    hwclock --systohc --utc
+    sudo ln -sf /usr/share/zoneinfo/Zone/SubZone /etc/localtime
+    sudo hwclock --systohc --utc
 fi
 
 # wireless (wifi-menu and autoconnect)
 netctl_service="netctl-auto@wlp2s0.service"
 if [[ `systemctl is-active $netctl_service` != "active" ]]; then
     $SYS_INSTALL iw wpa_supplicant dialog wpa_actiond
-    systemctl enable $netctl_service
+    sudo systemctl enable $netctl_service
 fi
 
 if [[ ! -e /home/$CREATE_USER ]]; then
-   useradd -m -g users -G wheel $CREATE_USER
+   sudo useradd -m -g users -G wheel -s /usr/bin/zsh $CREATE_USER
+   USER_MSG="User $CREATE_USER created. Create password with: passwd $CREATE_USER"
 fi
 
 if [[ ! -e /boot/intel-ucode.img ]]; then
     echo "installing ucode"
     UCODE_MSG="See https://wiki.archlinux.org/index.php/Microcode for setting up microcode"
-    $SYS_INSTALL intel-ucode 
+    $SYS_INSTALL intel-ucode
 fi
 
-# interface and user manager
+# Window Manger and basic functionality
 $SYS_INSTALL xorg-server xorg-xinit xorg-xev i3 i3lock \
-    rxvt-unicode xorg-xrdb urxvt-perls xclip ttf-dejavu
+    rxvt-unicode xorg-xrdb urxvt-perls xclip ttf-dejavu \
+    xf86-input-synaptics
 
 if [[ -e ~/.zlogin ]]; then
     ln -s $SCRIPTPATH/.zlogin ~/.zlogin
 fi
 
-# Use i3lock to lock the screen on lid close (i3 config handles timeout situation)
+## Use i3lock to lock the screen on lid close (i3 config handles timeout situation)
 if [[ `systemctl is-active i3lock.service` != "active" ]]; then
-    sudo cp $SCRIPTPATH/../etc/i3lock.service /etc/systemd/system/i3lock.service
+    sudo cp $SCRIPTPATH/etc/i3lock.service /etc/systemd/system/i3lock.service
     sudo systemctl enable i3lock.service
 fi
 
-# dev tools
+# Software
+## dev tools
 $SYS_INSTALL \
     zsh tmux vim \
     git \
-    openssh wget \
+    openssh wget rsync \
     cmake \
     python2
 
-# compression
+# TODO: Install yaourt
+
+## compression
 $SYS_INSTALL unace unrar zip unzip sharutils uudeview cabextract file-roller
 
-# usertools
+## usertools
 $SYS_INSTALL \
     firefox \
     apvlv
 
+echo "You need to set your own passwd with passwd"
 echo $UCODE_MSG
+echo $USER_MSG
 echo "See https://wiki.archlinux.org/index.php/Xorg for info on setting up video driver"
+if [[ ! -e /boot/grub ]]; then
+    echo "You need to install a bootloader still!!!"
+fi
 
