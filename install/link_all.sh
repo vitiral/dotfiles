e73577e0ad6b5e4d7a1bcd3add19c68b979e899e
Rett Berg <vitiral@gmail.com>
2020-11-07 20:53:17 -0700

    nixos works

--- install/link_all.sh
+++ install/link_all.sh
@@ -11,7 +11,6 @@ ln -s .dotfiles/.tmux.conf .
 ln -s .dotfiles/.zshrc .
 ln -s .dotfiles/.gitconfig .
 ln -s .dotfiles/.gitmodules .
-ln -s .dotfiles/.pydistutils.cfg .
 ln -s .dotfiles/vimrc .vimrc
 touch .vimrc.local.before
 touch .vimrc.local.after
@@ -21,6 +20,5 @@ mkdir -p .config
 cd .config
 ln -s $HOME/.dotfiles/config/i3 .
 ln -s $HOME/.dotfiles/config/i3status .
-ln -s $HOME/.dotfiles/config/novault.sites .
-ln -s $HOME/.dotfiles/config/alacritty .
-mkdir -p nvim && ln -s $HOME/.dotfiles/vimrc nvim/init.vim
+# ln -s $HOME/.dotfiles/config/novault.sites .
+# ln -s $HOME/.dotfiles/config/alacritty .
