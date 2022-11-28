5a0da6d2f5da6042d5e84286c7df69910904cb22
Rett Berg <vitiral@gmail.com>
2022-11-28 10:49:28 -0600

    minor updates

--- install/link_all.sh
+++ install/link_all.sh
@@ -1,24 +1,24 @@
 
 # std dotfiles
 cd ~
-ln -s .dotfiles/.xinitrc .
-ln -s .dotfiles/.Xresources .
-ln -s .dotfiles/.inputrc .
+# ln -s .dotfiles/.xinitrc .
+# ln -s .dotfiles/.Xresources .
+# ln -s .dotfiles/.inputrc .
 ln -s .dotfiles/.alias .
-ln -s .dotfiles/.bash_profile .
-ln -s .dotfiles/.bashrc .
+# ln -s .dotfiles/.bash_profile .
+# ln -s .dotfiles/.bashrc .
 ln -s .dotfiles/.tmux.conf .
 ln -s .dotfiles/.zshrc .
-ln -s .dotfiles/.gitconfig .
-ln -s .dotfiles/.gitmodules .
+# ln -s .dotfiles/.gitconfig .
+# ln -s .dotfiles/.gitmodules .
 ln -s .dotfiles/vimrc .vimrc
 touch .vimrc.local.before
 touch .vimrc.local.after
 
 # .config
-mkdir -p .config
-cd .config
-ln -s $HOME/.dotfiles/config/i3 .
-ln -s $HOME/.dotfiles/config/i3status .
+# mkdir -p .config
+# cd .config
+# ln -s $HOME/.dotfiles/config/i3 .
+# ln -s $HOME/.dotfiles/config/i3status .
 # ln -s $HOME/.dotfiles/config/novault.sites .
 # ln -s $HOME/.dotfiles/config/alacritty .
